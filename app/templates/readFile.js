43b7b1de0f64a74f146a2760fe7ca454b6a12716
lulu124579 <leelauren321@gmail.com>
2025-01-25 23:39:14 -0500

    Help

--- app/templates/readFile.js
+++ app/templates/readFile.js
@@ -1,9 +1,15 @@
 import data from './Recipe.json' with { type: 'json' };
+(function () {
 // JavaScript object
-const jsonData = data;
 
 // converting to JSON
-const obj = JSON.stringify(jsonData);
+const obj = data;
 
+const para = document.createElement("p");
+for (let i = 0; i < obj.length; i++) {
+    title = obj[i]["Title"]
+    const element = document.getElementById("Food");
+    element.appendChild(title);
+  }
 // accessing the data
-console.log(obj); // "{"name":"John","age":22}"
+})();
