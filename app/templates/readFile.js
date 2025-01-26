a810b6bf8ee975201d5f76b6703922ac32d23d42
lulu124579 <leelauren321@gmail.com>
2025-01-26 00:07:53 -0500

    I dunno what I just changed but whatever

--- app/templates/readFile.js
+++ app/templates/readFile.js
@@ -1,15 +1,14 @@
 import data from './Recipe.json' with { type: 'json' };
-(function () {
 // JavaScript object
 
 // converting to JSON
 const obj = data;
 
-const para = document.createElement("p");
-for (let i = 0; i < obj.length; i++) {
-    title = obj[i]["Title"]
-    const element = document.getElementById("Food");
-    element.appendChild(title);
+//const para = document.createElement("p");
+for (let i = 0; i < 31; i++) {
+    
+    console.log(obj[i]["Title"])
+   
   }
 // accessing the data
-})();
+
